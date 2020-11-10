5028b38fa1422457b05d1196f4905c7d87294231
Eshna <arfanoorchy@gmail.com>
2020-11-10 16:56:00 +0600

    midlab

--- dash.php
+++ dash.php
@@ -17,13 +17,45 @@
             <tr>
                 <th>SL.NO</th>
 				<tr>1</tr>
+				<tr>2</tr>
+				
+				
 				
                 <th>NAME</th>
+				<tr><h5>A dictonary of Architecture</h5></tr>
+				<tr><h5>Elephant kingdom Sculptures fron indian Architecture</h5></tr>
+				<tr><h5>Close to events -works of bikash Bhattacharjee </h5></tr>
+				
+				
+				
                 <th>PUBLISHER</th>
+				<tr>The Book Shop</tr>
+				<tr>Vikramjit Ram</tr>
+				<tr>Manasji Majumder</tr>
+				
                 <th>ISBN</th>
+				<tr>89564636</tr>
+				<tr>81-88204-6</tr>
+				<tr>978818973</tr>
+				
 				<th>PRICE</th>
+				<tr>500</tr>
+				<tr>1000</tr>
+				<tr>2000</tr>
+				
+				
 				<th>IMAGE</th>
+				<tr><img src = "img07.png" height = "80px" width = "80px"></tr>
+				<tr><img src = "img10.png" height = "80px" width = "80px"></tr>
+				<tr><img src = "img03.png" height = "80px" width = "80px"></tr>
+				
 				<th>DELETE</th>
+				<tr>
+				<tr><img src = "drop.png" height = "80px" width = "80px"></tr><br>
+				<tr><img src = "drop.png" height = "80px" width = "80px"></tr><br> 
+				<tr><img src = "drop.png" height = "80px" width = "80px"></tr>
+				
+				
             </tr>
             <?php
 
@@ -33,12 +65,14 @@
                 
                 foreach($verify as $var){
                     echo "<tr>";
-                    echo "<td>".$var['Name']."</td>";
+                    echo "<td>".$var['NAME']."</td>";
                     
-                    echo "<td>".$var['UserName']."</td>";
+                    echo "<td>".$var['PUBLISHER']."</td>";
                     
-                    echo "<td>".$var['Email']."</td>";
-                    echo "<td>".$var['Phone']."</td>";
+                    echo "<td>".$var['ISBN']."</td>";
+                    echo "<td>".$var['PRICE']."</td>";
+					 echo "<td>".$var['IMAGE']."</td>";
+					  echo "<td>".$var['DELETE']."</td>";
                     echo "</tr>";
                 }
                 
